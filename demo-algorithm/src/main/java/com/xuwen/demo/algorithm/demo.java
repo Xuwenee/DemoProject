684852e83f0d37113d08fa254655be7e5a5bb118
xuwen <xuwen@autohome.com.cn>
2020-03-10 22:58:40 +0800

    init

---
+++ demo-algorithm/src/main/java/com/xuwen/demo/algorithm/demo.java
@@ -0,0 +1,15 @@
+package com.xuwen.demo.algorithm;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author XuWen
+ * @created 2020-03-10 10:57 PM.
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+
+}
